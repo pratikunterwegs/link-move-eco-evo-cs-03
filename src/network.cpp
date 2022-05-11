7cd9d69903d80d281f19d1231736eac6b127b393
Pratik Gupte <pratikgupte16@gmail.com>
2022-05-11 14:22:54 +0200

    to calculate networks

---
+++ src/network.cpp
@@ -0,0 +1,31 @@
+/// functions for network operations
+
+#include "network.h"
+#include <Rcpp.h>
+
+/// function for dataframe from rcpp matrix
+Rcpp::DataFrame Network::getNtwkDf() {
+	std::vector<int> focal;
+	std::vector<int> subfocal;
+	std::vector<int> edgeWeight;
+
+	for (int n = 0; n < nVertices; ++n)
+	{
+		for(int m = n+1; m < nVertices; ++m) {
+			focal.push_back(n);
+			subfocal.push_back(m);
+			edgeWeight.push_back(adjMat(n, m));
+		}
+	}
+
+	assert(focal.size() == subfocal.size() && "ntwkdf: unequal lengths");
+
+	// make df
+	Rcpp::DataFrame ntwkDf = Rcpp::DataFrame::create(
+		Named("focal") = focal,
+		Named("target") = subfocal,
+		Named("assoc") = edgeWeight
+	);
+
+	return ntwkDf;
+}
---
+++ src/network.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <vector>
+#include <cassert>
+#include <Rcpp.h>
+
+using namespace Rcpp;
+
+/// the network structure, which holds an adjacency matrix
+// network should be a member of population later
+// network has funs to return network metrics and the adj matrix
+struct Network {
+public:
+    Network(const int popsize) :
+        nVertices(popsize),
+        adjMat (popsize, popsize)
+    {}
+    ~Network() {}
+
+    // members
+    const int nVertices;
+    Rcpp::NumericMatrix adjMat;
+
+    // functions
+    Rcpp::DataFrame getNtwkDf();
+    // std::vector<float> ntwkMeasures();
+    // std::vector<int> getDegree();
+};
+
